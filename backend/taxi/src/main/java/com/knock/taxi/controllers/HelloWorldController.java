030b7a3d5e292b101b31973fd1efb16f04be484f
Boris Georgiev <borisgeorgiev@Boriss-MacBook-Air.local>
2021-11-09 00:46:35 +0200

    Adding iOS & Java Spring Boot projects

---
+++ backend/taxi/src/main/java/com/knock/taxi/controllers/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.knock.taxi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class HelloWorldController {
+	
+	@GetMapping(value = "/public/helloworld")
+	public String PublicHello() {
+		return "Hello from the public API";
+	}
+	
+	@GetMapping(value = "/private/helloworld")
+	public String PrivateHello() {
+		return "Hello from the private API";
+	}
+}
---
+++ ios/Taxi/Shared/TaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiApp.swift
+//  Shared
+//
+//  Created by Boris Georgiev on 9.11.21.
+//
+
+import SwiftUI
+
+@main
+struct TaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
